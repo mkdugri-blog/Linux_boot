0061f3bde7ab44c923e85999b54d02c323e74ce6
Mk-dugri <mk98144m@gmail.com>
2025-08-20 00:18:01 +0530

    Convert to single page site - no routing needed

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,6 @@
 import { createRoot } from "react-dom/client";
-import { Router } from "wouter";
 import App from "./App";
 import "./index.css";
 
-// Simple routing for GitHub Pages
-createRoot(document.getElementById("root")!).render(
-  <Router>
-    <App />
-  </Router>
-);
+// Single page site - no routing needed
+createRoot(document.getElementById("root")!).render(<App />);
